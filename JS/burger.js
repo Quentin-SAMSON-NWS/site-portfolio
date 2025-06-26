ea68ea7213633fb37d2c91eca56999a2225f5988
Quentin-SAMSON-NWS <qsamson@normandiewebschool.fr>
2025-06-26 16:53:18 +0200

    burger

--- JS/burger.js
+++ JS/burger.js
@@ -1,16 +1,16 @@
 const btnOpen = document.querySelector('#btnOpen');
 const btnClose = document.querySelector('#btnClose');
+const menu = document.querySelector('.topnav_menu');
 
-function openMobilemenu(){
-  btnOpen.setAttribute('aria-expend', 'true');
+function openMobilemenu() {
+  btnOpen.setAttribute('aria-expanded', 'true');
+  menu.classList.add('active');
 }
 
-function closeMobilemenu(){
-  btnOpen.setAttribute('aria-expend', 'false');
+function closeMobilemenu() {
+  btnOpen.setAttribute('aria-expanded', 'false');
+  menu.classList.remove('active');
 }
 
-
-
-btnOpen.addEventListener('click',openMobilemenu);
-
-btnClose.addEventListener('click',openMobilemenu);
+btnOpen.addEventListener('click', openMobilemenu);
+btnClose.addEventListener('click', closeMobilemenu);
